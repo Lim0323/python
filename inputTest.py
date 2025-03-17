5fab71560054137a032b72be4db73514d9183295
jimin lim <lwlals323@gmail.com>
2025-03-17 11:53:23 +0900

    2주차 실습

---
+++ inputTest.py
@@ -0,0 +1,8 @@
+num1 = int(input("숫자1 ==>"))
+print(num1)
+
+num2 = int(input("숫자2 ==>"))
+print(num2)
+
+numSum = num1 + num2
+print(numSum)
---
+++ 변수.py
@@ -0,0 +1,21 @@
+num1 = 19
+num2 = 29
+resSum = num1 + num2
+print(num1,"+",num2,"=",resSum)
+
+num3 = 100
+num4 = 50
+빼기 = num3 - num4
+print(num3,"-",num4,"=",빼기)
+
+곱하기 = num3 * num4
+print(num3,"*",num4,"=",곱하기)
+
+나누기 = num3 / num4
+print(num3,"/",num4,"=",나누기)
+
+str1 = "Hello"
+str2 = "World"
+print(str1+str2)
+
+input()
